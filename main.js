ddf4adc47a745960dcf78ebefd114e9338463935
NinoBitsadze02 <ninobitsadze0202@gmail.com>
2025-09-14 15:44:26 +0400

    finished

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-const numberHere = document.getElementById("numberHere");
-const showButton = document.getElementById("showBtn");
+const userInput = document.getElementById("input");
+const checkButton = document.getElementById("checkBtn");
+const numHere = document.getElementById("numberHere");
+const generateButton = document.getElementById("generateBtn");
 const resetButton = document.getElementById("resetBtn");
 let number;
 
 function generateNumber() {
     number = Math.floor(Math.random() * 10);
-    numberHere.textContent = number;
+    numHere.textContent = "***";
+    userInput.value = "";
 }
 
-function startGame() {
-    generateNumber();
-    let guess;
-    while (guess !== number) {
-        guess = Number(prompt("Guess the number..."));
-        if (guess < number) {
-            numberHere.textContent = "Too low, try again.";
-        } else if (guess > number) {
-            numberHere.textContent = "Too high, try again.";
-        } else {
-            numberHere.textContent = `You guessed the number: ${number}`;
-        }
+generateButton.addEventListener("click", generateNumber);
+
+function checkGuess() {
+    const guess = Number(userInput.value);
+
+    if (guess < number) {
+        numHere.textContent = "Too low, try again";
+    } else if (guess > number) {
+        numHere.textContent = "Too high, try again";
+    } else if (guess === number) {
+        numHere.textContent = `You guessed correctly, the number is ${number}`;
+    } else {
+        numHere.textContent = "Please enter valid number";
     }
+    userInput.value = "";
 }
 
-showButton.addEventListener("click", startGame);
+checkButton.addEventListener("click", checkGuess);
+
 resetButton.addEventListener("click", () => {
-    numberHere.textContent = "***";
+    numHere.textContent = "***";
+    userInput.value = "";
 });
+
+
+
 
